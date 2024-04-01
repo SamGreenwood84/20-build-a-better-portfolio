95b26f3b3742c2242280923f2bb8d3e81c9d0909
your-SamGreenwood <your-sagg_84@hotmail.com>
2024-04-01 15:33:07 -0300

    completed about me section

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AboutMe from './components/aboutMe/aboutMe';
+
+const App = () => {
+  return (
+    <div className="App">
+      <AboutMe />
+      {/* Render other sections/components here */}
+    </div>
+  );
+};
+
+export default App;
