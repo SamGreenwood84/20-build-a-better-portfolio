1303d8b9bf958229f15e3c7869298ead96d603fe
your-SamGreenwood <your-sagg_84@hotmail.com>
2024-04-01 15:54:48 -0300

    added header and nav components

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import './App.css'; // Import CSS file for global styles
 import AboutMe from './components/aboutMe/aboutMe'; // Import AboutMe component
 import Contact from './components/contact/contact'; // Import Contact component
 import ContactForm from './components/contactForm/contactForm'; // Import ContactForm component
+import Footer from './components/footer/footer'; // Import Footer component
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Contact />
       <ContactForm />
       {/* Render other sections/components here */}
+      <Footer />
     </div>
   );
 };
