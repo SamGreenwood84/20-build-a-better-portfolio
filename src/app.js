be33703244377ec8722b54fee36651a5a55e72d2
your-SamGreenwood <your-sagg_84@hotmail.com>
2024-04-01 15:46:06 -0300

    added contact and contact form to app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import AboutMe from './components/aboutMe/aboutMe';
+import Contact from './components/contact/contact'; // Import Contact component
+import ContactForm from './components/contactForm/contactForm'; // Import ContactForm component
 
 const App = () => {
   return (
     <div className="App">
       <AboutMe />
       {/* Render other sections/components here */}
+      <Contact />
+      <ContactForm />
     </div>
   );
 };
